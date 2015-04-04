40b7963ff3f0c85110ea6fcf12471d9f79e58e19
laurence <liloug.0873@gmail.com>
2015-04-04 18:12:49 +0200

    moteur de recherche modifé

--- formulaireRecherche/function2.php
+++ formulaireRecherche/function2.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+ 
+ <html lang="fr" xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+		<title> moteur de recherche </title>
+	</head>
+	<body>
+
 <?php
 	$host = "localhost";
 	$user = "root";
@@ -7,26 +17,28 @@
 		if ($Bibli->connect_errno) {
 		echo "Echec lors de la connexion à MySQL : (" . $Bibli->connect_errno . ") " . $Bibli->connect_error;
 	}
+
 	$Bibli->set_charset('utf8');
 
 ?>
+
 
 <form method="POST" >
 <label for="author"> Auteur : </label> <input type="text" name="author"> <br /> 
 <label for="title"> Titre : </label>  <input type="text" name="title"><br /> 
 <label for="genre"> Genre : </label> <select name="genre"> 
 <option> </option>
-<option value="h"> Histoire </option>
-<option value="r"> Roman </option>
-<option value="e"> Essai </option>
-<option value="p"> Poésie </option>
-<option value="c"> Conte </option>
-<option value="d"> Dictionnaire </option>
-<option value="b"> Biographie </option>
-<option value="a"> Auto-biographie </option>
-<option value="t"> théatre </option>
+<option value="histoire"> Histoire </option>
+<option value="roman"> Roman </option>
+<option value="essai"> Essai </option>
+<option value="poesie"> Poesie </option>
+<option value="comte"> Conte </option>
+<option value="dictionnaire"> Dictionnaire </option>
+<option value="biographie"> Biographie </option>
+<option value="auto-biographie"> Auto-biographie </option>
+<option value="theatre"> theatre </option>
 <br/>
-<input type="submit" value="chercher" name="submit">
+<input type="submit" value="rechercher" name="submit">
 </form>
 
 
@@ -46,28 +58,11 @@ if (isset($_POST['title'], $_POST['author']) && (!empty($_POST['title'])) && (!e
 	
 	while ($stmt->fetch())
 	{
-		printf("%s %s %s\n", $nomAuteur, $prenomAuteur, $titre);
-		echo "<br/>";
+			echo "Auteur : ".$nomAuteur." ".$prenomAuteur;
+            echo "<br /> Titre :  ".$titre;
+            echo "<br />";
 	}
 	
-
-}
-
-else if (isset($_POST['author']) && (!empty($_POST['author'])) )
-{
-
-$stmt = $Bibli->prepare('SELECT nomAuteur, prenomAuteur, titre 
-						 FROM livre NATURAL JOIN ecrit NATURAL JOIN auteur 
-						 WHERE lower(nomAuteur) = ?');
-$stmt->bind_param("s", $_POST['author']);
-$stmt->execute();
-$stmt->bind_result($nomAuteur, $prenomAuteur, $titre);
-
-	while ($stmt->fetch())
-	{
-		printf("%s %s %s\n", $nomAuteur, $prenomAuteur, $titre);
-		echo "<br/>";
-	}
 
 }
 else if (isset($_POST['title']) && (!empty($_POST['title'])) )
@@ -82,35 +77,73 @@ else if (isset($_POST['title']) && (!empty($_POST['title'])) )
 		$stmt->bind_result($nomAuteur, $prenomAuteur, $titre);
 			while ($stmt->fetch())
 			{
-				printf(" %s %s %s\n", $nomAuteur, $prenomAuteur, $titre);
-				echo "<br/>";
+				 echo "Auteur : ".$nomAuteur." ".$prenomAuteur;
+                 echo "<br /> Titre :  ".$titre;
+                 echo "<br />";
 			}
 	}
 }
-
-elseif (isset($_POST['genre']) && (!empty($_POST['genre'])) )
+else if (isset($_POST['genre'], $_POST['author']) && (!empty($_POST['genre'])) && (!empty($_POST['author'])) )
 {
-		$stmt = $Bibli ->prepare ('SELECT nomAuteur, prenomAuteur, titre, anneeEdition 
+		$stmt = $Bibli->prepare('SELECT nomAuteur, prenomAuteur, titre 
+							 FROM genre NATURAL JOIN livre NATURAL JOIN ecrit NATURAL JOIN auteur 
+							 WHERE lower(libelleGenre) LIKE lower(?) AND lower(nomAuteur) = ? ');
+		$genre=$_POST['genre'];
+		$aut= $_POST['author'];
+		$stmt->bind_param("ss", $genre, $aut);
+		$stmt->execute();
+		$stmt->bind_result($nomAuteur, $prenomAuteur, $titre);
+	
+			while ($stmt->fetch())
+			{
+				 echo "Auteur : ".$nomAuteur." ".$prenomAuteur;
+                 echo "<br /> Titre :  ".$titre;
+                 echo "<br />";
+			}
+}	
+else if (isset($_POST['author']) && (!empty($_POST['author'])) )
+{
+
+		$stmt = $Bibli->prepare('SELECT nomAuteur, prenomAuteur, titre 
+								 FROM livre NATURAL JOIN ecrit NATURAL JOIN auteur 
+								 WHERE lower(nomAuteur) = ?');
+		$stmt->bind_param("s", $_POST['author']);
+		$stmt->execute();
+		$stmt->bind_result($nomAuteur, $prenomAuteur, $titre);
+
+			while ($stmt->fetch())
+			{
+				echo "Auteur : ".$nomAuteur." ".$prenomAuteur;
+        		echo "<br /> Titre :  ".$titre;
+        		echo "<br />";
+			}
+
+}	
+else if (isset($_POST['genre']) && (!empty($_POST['genre'])) )
+{
+		$stmt = $Bibli->prepare ('SELECT nomAuteur, prenomAuteur, titre, anneeEdition 
 								   FROM genre NATURAL JOIN livre NATURAL JOIN ecrit NATURAL JOIN auteur 
 								   WHERE lower(libelleGenre) LIKE lower(?) ');
 		
-		$gre= "%".$_POST['genre']."%";
-		$stmt->bind_param("s", $gre);
+		$stmt->bind_param("s", $_POST['genre']);
 		$stmt->execute();
 		$stmt->bind_result($nomAuteur, $prenomAuteur, $titre, $anneeEdition);
-			while($stmt ->fetch())
+			while($stmt->fetch())
 			{
-				printf("%s %s %s %s\n", $nomAuteur, $prenomAuteur, $titre, $anneeEdition);
-				echo "<br>";
+				 echo "Auteur : ".$nomAuteur." ".$prenomAuteur;
+                 echo "<br /> Titre :  ".$titre;
+                 echo "<br />";
 			}
+				
 }
 else
 {
-	echo "Vous devez saisir une recherche.";
+	echo "Veuillez saisir votre recherche.";
 }
 
-
-
 ?>
+
+</body>
+</html>
 
 
