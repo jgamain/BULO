daf85faed024063938946bd5fe7705691434cf71
jgamain <jeanne.gamain@gmail.com>
2015-04-07 17:34:40 +0200

    pageLivre

--- pageLivre.php
+++ pageLivre.php
@@ -71,37 +71,139 @@
 		</div>
 		
 		
+		<!-- RECHERCHE -->
+		<?php
+			include "connectBibli.php";
+			
+		?>
+		<h3><a href="#" class="couleur">Recherche</a></h3><br />
+		<form class="form-horizontal hidden" method="post" action="resultatRecherche.php">
+			<div class="form-group">
+				<label for="author" class="col-sm-1 control-label">Auteur</label>
+				<div class="col-xs-6">
+					<input type="text" class="form-control" id="author" name="author" placeholder="Nom de l'auteur">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="title" class="col-sm-1 control-label">Titre</label>
+				<div class="col-xs-6">
+					<input type="text" class="form-control" id="title" name="title" placeholder="Titre du livre">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="genre" class="col-sm-1 control-label">Genre</label>
+				<div class="col-xs-6">
+					<select class="form-control" id="genre" name="genre">
+						<option value="">-- Indéfini --</option>
+						<?php
+								$result = $Bibli->query("SELECT libelleGenre FROM genre");
+								while($row = $result->fetch_assoc()){
+									echo "<option value='".$row['libelleGenre']."'>".$row['libelleGenre']."</option>";
+								}
+						?>
+					</select>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-1 col-sm-offset-1">
+				<button type="submit" class="btn btn-violet">Rechercher</button>
+				</div>
+			</div>
+		</form>
+		
+		
 		<!-- INFORMATIONS SUR LE LIVRE -->
-		
-			<?php
-
-	include "connectBibli.php";
-	$stmt = $Bibli->prepare('SELECT titre, nomAuteur, prenomAuteur, nomTraducteur, prenomTraducteur, nomEditeur, nomCollection, anneeEdition, libelleLangue, libelleGenre, nbPage, description
-							 FROM livre NATURAL JOIN ecrit NATURAL JOIN auteur NATURAL LEFT JOIN traduit NATURAL LEFT JOIN traducteur NATURAL JOIN edite NATURAL JOIN editeur NATURAL LEFT JOIN collection NATURAL JOIN est_ecrit_en NATURAL JOIN langue NATURAL LEFT JOIN genre
-							  WHERE numLivre= ?');
-	
-	$stmt->bind_param("i", $_GET['numero']);
-	$stmt->execute();
-	$stmt->store_result();
-	$stmt->bind_result($titre, $nomAuteur, $prenomAuteur, $nomTraducteur, $prenomTraducteur, $nomEditeur, $nomCollection, $anneeEdition, $libelleLangue, $libelleGenre, $nbPage, $description);
-	
-		if($stmt->num_rows == 0)
-		{
-			echo "Livre introuvable";
+		<div>
+		<?php
+		if(isset($_GET['numero'])){
+			$_GET['numero'] = (int)$_GET['numero'];
+			if($_GET['numero'] != 0){
+				include "connectBibli.php";
+				
+				$stmt = $Bibli->prepare('SELECT titre, anneeEdition, nomCollection, libelleGenre, nbPage, description
+										 FROM livre NATURAL LEFT JOIN collection NATURAL LEFT JOIN genre
+										  WHERE numLivre= ?');
+				$stmt->bind_param("i", $_GET['numero']);
+				$stmt->execute();
+				$stmt->store_result();
+				$stmt->bind_result($titre, $anneeEdition, $nomCollection, $libelleGenre, $nbPage, $description);
+				if($stmt->num_rows != 0)
+				{
+					$stmt->fetch();
+					
+					echo "<h2>Titre :  ".$titre."</h2>";
+					
+					// Récupération de la liste d'auteurs
+					$stmtA = $Bibli->prepare('SELECT nomAuteur, prenomAuteur
+												FROM ecrit NATURAL JOIN auteur
+												WHERE numLivre = ?');
+					$stmtA->bind_param("i", $_GET['numero']);
+					$stmtA->execute();
+					$stmtA->bind_result($nomAuteur, $prenomAuteur);
+					$stmtA->fetch();
+					echo "<br /><strong>Auteur(s) : </strong>".$nomAuteur." ".$prenomAuteur;
+					while($stmtA->fetch()){
+						echo " / ".$nomAuteur." ".$prenomAuteur;
+					}
+					
+					// Récupération de la liste de traducteurs
+					$stmtT = $Bibli->prepare('SELECT nomTraducteur, prenomTraducteur
+												FROM traduit NATURAL JOIN traducteur
+												WHERE numLivre = ?');
+					$stmtT->bind_param("i", $_GET['numero']);
+					$stmtT->execute();
+					$stmtT->store_result();
+					$stmtT->bind_result($nomTraducteur, $prenomTraducteur);
+					if($stmtT->num_rows != 0){
+						$stmtT->fetch();
+						echo "<br />Traducteur(s) : ".$nomTraducteur." ".$prenomTraducteur;
+						while($stmtT->fetch()){
+							echo " / ".$nomTraducteur." ".$prenomTraducteur;
+						}
+					}
+					
+					//Récupération de la liste de langues
+					$stmtL = $Bibli->prepare('SELECT libelleLangue
+												FROM est_ecrit_en NATURAL JOIN langue
+												WHERE numLivre = ?');
+					$stmtL->bind_param("i", $_GET['numero']);
+					$stmtL->execute();
+					$stmtL->bind_result($libelleLangue);
+					$stmtL->fetch();
+					echo "<br />Ecrit en : ".$libelleLangue;
+					while($stmtL->fetch()){
+						echo " / ".$libelleLangue;
+					}
+					
+					if(isset($nomCollection)){
+						echo "<br /> Collection : ".$nomCollection;
+					}
+					//Récupération de la liste d'éditeurs
+					$stmtE = $Bibli->prepare('SELECT nomEditeur
+												FROM edite NATURAL JOIN editeur
+												WHERE numLivre = ?');
+					$stmtE->bind_param("i", $_GET['numero']);
+					$stmtE->execute();
+					$stmtE->bind_result($nomEditeur);
+					$stmtE->fetch();
+					echo "<br />Editeur(s) : ".$nomEditeur;
+					while($stmtE->fetch()){
+						echo " / ".$nomEditeur;
+					}
+					
+					echo "<br />Année d'édition : ".$anneeEdition."<br />";
+				}
+				else{
+					echo "Livre introuvable";
+				}
+			}
+			else{
+				echo "Livre introuvable.";
+			}
 		}
-	
-	while ($stmt->fetch())
-	{
-			echo "Auteur : ".$nomAuteur." ".$prenomAuteur;
-            echo "<br /> Titre :  ".$titre;
-            echo "<br /> Traducteur : ".$nomTraducteur." ".$prenomTraducteur;
-            echo "<br /> Collection : ".$nomCollection;
-            echo "<br /> Editeur : ".$nomEditeur." Année d'édition : ".$anneeEdition;
-            echo "<br>";
-          
-	}
+
 	?>
-		
+		</div>
 		
 		<script src="bootstrap/js/jquery.js"></script>
 		<script src="bootstrap/js/bootstrap.js"></script>
