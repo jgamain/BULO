80b10275fd2d2806f98d919e5bd0c848081bfcf3
jgamain <jeanne.gamain@gmail.com>
2015-04-14 15:50:38 +0200

    début connexion compte

--- index.php
+++ index.php
@@ -18,64 +18,9 @@
   <body>
 	<div class="container">
 		<!-- HEADER -->
-		<div class="header row">
-			<div class="navbar navbar-default navbar-fixed-top">
-				<div class="navbar-inner">
-					<h1 class="navbar-header">
-						<a class="navbar-brand" href="index.html">
-							<img src="images/logo.jpg" alt="BULO" class="logo">
-						</a>
-					</h1>
-					<ul class="nav navbar-nav pull-right">
-						<li class="active">
-							<a href="index.html">
-								<span class="glyphicon glyphicon-home" aria-hidden="true"></span>
-								<br />Accueil
-							</a>
-						</li>
-						<li>
-							<a href="catalogue.php">
-								<span class="glyphicon glyphicon-book" aria-hidden="true"></span>
-								<br />Catalogue
-							</a>
-						</li>
-						<li class="popover-markup">
-							<a href="#" class="trigger" data-placement="bottom">
-								<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-								<br />Se connecter
-								<div class="content hide">
-									<form method="post" class="form-popover">
-										<div class="form-group">
-											<label for="login">Login</label>
-											<input type="text" class="form-control" id="login" placeholder="Entrez votre login">
-										</div>
-										<div class="form-group">
-											<label for="pwd">Mot de passe</label>
-											<input type="text" class="form-control" id="pwd" placeholder="Entrez votre mot de passe">
-										</div>
-										<div class="form-group">
-											<button type="button" class="btn btn-info">Connexion</button>
-										</div>
-									</form>
-									<a href="#">Vous n'êtes pas encore inscrit ?</a>
-								</div>
-							</a>
-						</li>
-						<li class="dropdown">
-							<a data-toggle="dropdown">
-								<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
-								<br />Infos pratiques<b class="caret"></b>
-							</a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Horaires et accès</a></li>
-								<li><a href="#">Règlement intérieur et tarifs</a></li>
-								<li><a href="#">Nous contacter</a></li>
-							</ul>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
+		<?php
+			include "header.html";
+		?>
 		<!--CAROUSEL-->
 		<div class="row">
 			<h1 class="header couleur">Nouveautés</h1>
