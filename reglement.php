a64e3c2ea486ff7c4a79419647ab59895aac4cb3
laurence <liloug.0873@gmail.com>
2015-04-21 22:52:36 +0200

    pages statiques horaires, règlement, maps...
    c’est un 1er jet. ca pourrait être sympa de mettre les titre dans la
    meme couleur que le titre nouveautés sur la page d’accueil du site ?

---
+++ reglement.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+ 
+ <html lang="fr" xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<title> Règlement intérieur </title>
+		
+	</head>
+	<body>
+		<h1> <center> Règlement intérieur de la bibliothèque </center> </h1>
+
+		<h2> Accès et inscription </h2>
+		
+		<DL>
+			<DT> Conditions d'accès </DT>
+			<br>
+			<DD>L’accès à la bibliothèque est réservé en priorité aux étudiants et personnels de l’Université Paris Sud et aux lecteurs inscrits, et dans la limite des places disponibles, à toute personne qui souhaite consulter les collections.
+			L’accès aux salles de lecture et la consultation des ouvrages est libre et gratuit.
+			</DD>
+			<br>
+			<DT> Inscription gratuite</DT>
+			<br>
+			<DD>
+				<DD>Etudiants de l’Université Paris-Sud</DD>
+ 				<br>
+				<DD> L’inscription administrative inclut automatiquement l’inscription à la bibliothèque. La carte d’étudiant sert de carte de bibliothèque. 
+				</DD>
+ 				<br>
+				<DD>Enseignants-chercheurs et personnels de l’Université Paris Sud</DD>
+ 				<br>
+				<DD>L’inscription à la bibliothèque des enseignants-chercheurs, chercheurs et personnels se fait de façon automatique. Elle nécessite l’établissement d’une carte de bibliothèque lors du premier emprunt.
+				</Dd>
+ 				<br>
+ 			<DT> Inscription payante </DT>
+ 				<br>
+				<DD>
+          		Etudiants extérieurs à l’Université Paris-Sud <br>
+          		Enseignants et chercheurs extérieurs à l’Université Paris-Sud <br>
+          		Particuliers <br>
+				</DD>
+				</ul>
+			</DD>
+			<br>
+		</DL>
+
+		<h2> Services </h2>
+
+		<DL>
+			<DT> Consultation des collections </DT>
+			<br>
+			<DD>Les collections en libre-accès peuvent être consultées sans formalités.
+			Les collections conservées en magasins sont communiquées, sur demande, en banque d'accueil en échange d'une pièce d'identité ou de la carte d'étudiant.
+			</DD>
+			<br>	
+			<DT> Documentation électonique </DT>
+			<br>
+			<DD>La Bibliothèque met à disposition de ses usagers des documents électroniques accessibles depuis des postes informatiques disponibles au sein des bibliothèques, par connexion Wifi et via des tablettes.
+			</DD>
+			<br>
+		</DL>
+
+		<h2> Respect des documents et des biens</h2>
+
+		<DL>
+			<DT> Respect des documents </DT>
+			<br>
+			<DD>Il est demandé aux usagers de prendre soin des documents qui leur sont communiqués ou prêtés.<br>
+			Les collections des bibliothèques étant un bien commun, il est strictement interdit d’écrire, de dessiner ou de faire une quelconque marque sur les documents, de plier ou de corner les pages.<br>
+			Afin d’assurer la longévité des collections, les usagers doivent s’abstenir de toute intervention sur les documents. Les lecteurs qui remarqueraient des documents en mauvais état sont invités à les signaler aux bibliothécaires qui feront effectuer la réparation par un professionnel.
+			</DD>
+			<br>
+			<DT> Contrôle antivol </DT>
+			<br>
+			<DD> Tous les documents sont équipés contre le vol. Les usagers sont responsables de tous les documents trouvés en leur possession : ils doivent s’assurer avant de franchir le contrôle antivol qu’ils n’ont sur eux aucun document dont le prêt n'a pas été préalablement enregistré. Si le système de détection se déclenche lors de leur passage, ils doivent se présenter à la banque de prêt et remettre leur carte de bibliothèque. Ils sont invités à se prêter aux contrôles des sacs, sacoches et porte-documents en leur possession par le personnel de la bibliothèque concernée. 
+			</DD>
+			<br>
+			<DT> Pénalités en cas de retard </DT>
+			<br>
+			<DD> Tout retard dans la restitution des documents entraînera une suspension du prêt à domicile dans l’ensemble des bibliothèques du Service Commun de la Documentation égale au nombre de jours de retard.
+			Des lettres de rappel sont envoyées aux lecteurs en retard sur leur adresse mail. Tout changement d’adresse doit être signalé à la bibliothèque.
+			En cas de non-restitution ou perte d’un document, l’emprunteur devra le racheter ou le rembourser d’après un devis établi par la bibliothèque correspondant au prix d’achat TTC de la dernière édition ou au prix réactualisé s’il n’est plus édité.
+			</DD>
+			
+		</DL>
+
+	</body>
+	</html>
